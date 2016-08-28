821b53aecda44010c1fe228a7bf1f4a8a6226d3b
Toms Astahovskis <astmens@users.noreply.github>
2016-08-28 17:49:20 +0200

    Initial commit of my portfolio page. Also hosted on CodePen.io (http://codepen.io/astmens/pen/ZOgLQa).

---
+++ README.md
@@ -0,0 +1,2 @@
+My portfolio page. Also hosted on CodePen.io (http://codepen.io/astmens/pen/ZOgLQa) as part of freeCodeCamp.com project.
+"# portfolioPage" 
---
+++ portfolioPage.js
@@ -0,0 +1,9 @@
+/* http://stackoverflow.com/questions/31536467/how-to-hide-drawer-upon-user-click 
+function close() {
+  var d = document.querySelector('.mdl-layout');
+  d.MaterialLayout.toggleDrawer();
+}
+
+document.querySelector('.mdl-layout__drawer').addEventListener('click', close);
+
+*/
